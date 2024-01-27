5e58558aa00419767d563125791a1c7d6be900d9
adityaslyf <aditya.varshneymail@gmail.com>
2024-01-27 17:09:40 +0530

    updated feed and post section with multiple post using props

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,16 +1,17 @@
 import "./Feed.css";
 import Share from "../Share/Share";
 import Post from "../Post/Post";
-// import Posts from "../../DummyData";
+import {Posts} from '../../DummyData'
 const Feed = () => {
   return (
-    <div className="feed bg-sky-100">
+    <div className="feed bg-sky-100 ">
       <div className=" feedwrapper">
         <Share />
-        {/* {Posts.ma((p) => (
+        {Posts.map((p) => (
           <Post key={p.id} post={p} />
-        ))} */}
-        <Post />
+        ))}
+      
+     
       </div>
     </div>
   );
